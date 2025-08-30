77cd730535bb68940195346a38bf57b39dbf7fe9
Raneem Hamarneh <raneem99hamarneh@gmail.com>
2025-08-30 04:53:26 +0300

    Add initial JavaScript file with text toggle functionality

--- changertext.js
+++ changertext.js
@@ -1,5 +1,6 @@
 let div = document.getElementById("main");
 let displayed = 0;
+let displayed1 = 0;
 
 function hideShow() {
     if (displayed == 1) {
@@ -8,5 +9,19 @@ function hideShow() {
     } else {
         div.style.display = "none";
         displayed = 1;
+    }
+}
+
+let p = document.getElementById("main2")
+
+function changeText() {
+    if (displayed1 == 1) {
+        // show original text
+        p.innerHTML = "I can Change By clicking on the button";
+        displayed1 = 0;
+    } else {
+        // change to new text
+        p.innerHTML = "New text!<br>Hello World";
+        displayed1 = 1;
     }
 }
